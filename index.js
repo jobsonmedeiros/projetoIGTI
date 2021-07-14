5e1c0a025804cb9d4a89c601e70273dee61f1e2b
jobsonmedeiros <jobsonmm@gmail.com>
2021-07-14 01:40:09 -0300

    Criada a rota Contato

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const app = express() // (2) clona todo o módulo express e o atribui à constan
 // (4) Agora que já iniciamos o servidor vamos indicar uma pasta que queremos abrir
 // (5) Barra pra indicar que é página raiz
 app.get("/", function(req, res){
-    // aqui dentro da função vamos colocar tudo o que esta página vai receber
-    res.send("Página Sobre")
-    // Agora podemos acessar localhost:8080 no navegador
+    // (6) aqui dentro da função vamos colocar tudo o que esta página vai receber
+    res.send("Descrição da Página...")
+    // (7) Agora podemos acessar localhost:8080 no navegador
 }) 
+
+// (8) Agora vamos criar a rota contato
+app.get("/contato", function(req, res){
+    res.send("Página de Contato")
+})
+
 // (3) Agora vamos iniciar um servidor
 app.listen(8080)
